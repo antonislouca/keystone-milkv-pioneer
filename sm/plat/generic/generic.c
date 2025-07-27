e6e7c17f8bcbadc4913f94712d6a5f0589abb4ce
antonislouca <antonis.loucaa@gmail.com>
2025-07-27 12:52:49 +0200

    upgrading opensbi

--- sm/plat/generic/generic.c
+++ sm/plat/generic/generic.c
@@ -2,13 +2,33 @@
 #include <platform_override.h>
 #include <sbi_utils/fdt/fdt_fixup.h>
 #include <sbi_utils/fdt/fdt_helper.h>
+#include <sbi_utils/ipi/aclint_mswi.h>
 
 #include "sm.h"
 
 static int generic_final_init(bool cold_boot, const struct fdt_match *match) {
+  // void *fdt;
   sm_init(cold_boot);
+  // if (!cold_boot)
+  //   return 0;
+  // fdt = fdt_get_address();
+  // fdt_fixups(fdt);
   return 0;
 }
+/*
+ * Initialize IPI for current HART.
+ */
+// static int generic_ipi_init(bool cold_boot) {
+//   int ret;
+//
+//   if (cold_boot) {
+//     ret = aclint_mswi_cold_init(&mswi);
+//     if (ret)
+//       return ret;
+//   }
+//
+//   return aclint_mswi_warm_init();
+// }
 
 static const struct fdt_match generic_match[] = {
     {.compatible = "riscv-virtio"},
@@ -21,5 +41,6 @@ static const struct fdt_match generic_match[] = {
     {},
 };
 
-const struct platform_override generic = {.match_table = generic_match,
-                                          .final_init = generic_final_init};
+const struct platform_override generic = { //.ipi_init = generic_ipi_init,
+    .match_table = generic_match,
+    .final_init = generic_final_init};
