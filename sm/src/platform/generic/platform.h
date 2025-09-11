00d00b1af20b60b66a0b7137cbddf94f2045d261
antonislouca <antonis.loucaa@gmail.com>
2025-09-11 13:01:05 +0200

    changes to compile linux kernel

--- sm/src/platform/generic/platform.h
+++ sm/src/platform/generic/platform.h
@@ -3,27 +3,24 @@
 #define _PLATFORM_H_
 
 // No special data needed for default platform
-struct platform_enclave_data{
-
-};
+struct platform_enclave_data {};
 
 // Enclave configuration
-#define ENCL_MAX                16
-#define ENCLAVE_REGIONS_MAX     8
+#define ENCL_MAX 16
+#define ENCLAVE_REGIONS_MAX 8
 
 // SM configuration
-#define SMM_BASE                0x80000000
-#define SMM_SIZE                0x200000
+#define SMM_BASE 0x80000000
+#define SMM_SIZE 0x200000
 
 // PMP configuration
-#define PMP_N_REG               8
-#define PMP_MAX_N_REGION        16
+#define PMP_N_REG 8
+#define PMP_MAX_N_REGION 16
 
 // CPU configuration
-#define MAX_HARTS               16
+#define MAX_HARTS 64
 
 // Initialization functions
 void sm_copy_key(void);
 
 #endif // _PLATFORM_H_
-
